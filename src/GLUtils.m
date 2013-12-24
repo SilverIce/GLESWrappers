ff6cb41310980f5a892ae9c11acdeba347aeae43
slv <slifeleaf@gmail.com>
2013-12-24 16:02:24 +0200

    improve gl error code output - print string

--- src/GLUtils.m
+++ src/GLUtils.m
@@ -7,7 +7,31 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <OpenGLES/ES1/gl.h>
+#import <GLKit/GLKit.h>
+
+#define GL_ERROR_CODE(code)     {(GLenum)(code), #code}
+
+static const struct {
+    GLenum          code;
+    const GLchar    *codeString;
+} GLErrorCodeInfo[] = {
+    GL_ERROR_CODE(GL_INVALID_ENUM),
+    GL_ERROR_CODE(GL_INVALID_VALUE),
+    GL_ERROR_CODE(GL_INVALID_OPERATION),
+    GL_ERROR_CODE(GL_STACK_OVERFLOW),
+    GL_ERROR_CODE(GL_STACK_UNDERFLOW),
+    GL_ERROR_CODE(GL_OUT_OF_MEMORY),
+};
+
+static const GLchar * GLErrorCodeString(GLenum code) {
+    for (GLuint i = 0; sizeof(GLErrorCodeInfo)/sizeof(GLErrorCodeInfo[0]); ++i) {
+        if (GLErrorCodeInfo[i].code == code) {
+            return GLErrorCodeInfo[i].codeString;
+        }
+    }
+    
+    return "no error code related string";
+}
 
 void GLassertStateValid() {
 #warning TODO: remove that definition in future as it may reduce performance
@@ -16,7 +40,7 @@ void GLassertStateValid() {
 #ifdef GLES_DEBUG
     GLenum error = glGetError();
     if (error != GL_NO_ERROR) {
-        NSLog(@"gl error: 0x%x", error);
+        NSLog(@"gl error: 0x%x - %s", error, GLErrorCodeString(error));
         assert(false);
     }
 #endif
